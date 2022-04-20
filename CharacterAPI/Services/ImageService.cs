5e8fa535792dda06476291ec78022c0dd9f08081
Jake Saint Germain <jakesaint27@gmail.com>
2022-04-20 19:16:59 -0400

    comment clean up

--- CharacterAPI/Services/ImageService.cs
+++ CharacterAPI/Services/ImageService.cs
@@ -13,19 +13,12 @@ namespace CharacterAPI.Services
             {
                 Directory.CreateDirectory(CharacterImages);
 
-                CopyFilesRecursively(BaseImages, CharacterImages);
+                CopyImageFiles(BaseImages, CharacterImages);
             };
         }
 
-        private static void CopyFilesRecursively(string sourcePath, string targetPath)
+        private static void CopyImageFiles(string sourcePath, string targetPath)
         {
-            //Now Create all of the directories
-            //foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
-            //{
-            //    Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
-            //}
-
-            //Copy all the files & Replaces any files with the same name
             foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
             {
                 File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
